872e27ccb1f04df4b788b7566711cc96f41ab3a0
Peter <peter.tran2@ucalgary.ca>
2025-03-05 18:34:09 -0700

    title fixing

--- TableTalk/src/pages/Suggest.jsx
+++ TableTalk/src/pages/Suggest.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Suggest() {
   return (
     <div className="suggest-container">
-      <h1>Restaurant Suggestions</h1>
+      <h1>TableTalk Suggestions</h1>
     </div>
   );
 } 
