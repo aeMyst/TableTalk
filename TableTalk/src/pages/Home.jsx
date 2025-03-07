07d1328fff4655e40ccae0ce314e3e29954614aa
Peter <peter.tran2@ucalgary.ca>
2025-03-06 21:01:55 -0700

    added logo for homepage and some basic new colors

--- TableTalk/src/pages/Home.jsx
+++ TableTalk/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import "../elements/card.css"
+import "../elements/card.css";
+import "./Home.css";
+import mainLogo from "../assets/logo/mainLogo.png";
 
 export default function Home() {
   return (
     <div className="home-container">
       <div className="card">
+        <div className="logo-container">
+          <img src={mainLogo} alt="TableTalk Logo" className="main-logo" />
+        </div>
         <h1>Welcome to TableTalk</h1>
       </div>
     </div>
