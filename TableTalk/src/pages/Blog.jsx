9307cf45d63425c1699651d5b4e6c14ead940a40
YDancer <149452536+YDancer@users.noreply.github.com>
2025-03-13 11:38:06 -0600

    Clicking works

--- TableTalk/src/pages/Blog.jsx
+++ TableTalk/src/pages/Blog.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "../elements/card.css";
 import "./Blog.css";
 import menuHidden from "../assets/svg/menuHidden.svg";
@@ -10,6 +10,13 @@ import replyImg from "../assets/svg/reply.svg";
 
 export default function Blog() {
   
+  const[post1State, setPost1State] = useState(false);
+
+  function openMenu(){
+    setPost1State(prevState=>!prevState);
+    
+  }
+
   return (
     <div className="blog-container">
       <div className="card">
@@ -19,14 +26,14 @@ export default function Blog() {
               <h1 className ="sidebarTitleText">Topics</h1>
             </div>
             <div className="sidebarContent">
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Casting Shadows</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Catan</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Chess</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Clue</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Exploding Kittens</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Monopoly</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Pandemic</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Scrabble</a><br></br>
+              <a href="#" className="topics firstTopic">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Casting Shadows</a><br></br>
+              <a href="#" className="topics">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Catan</a><br></br>
+              <a href="#" className="topics">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Chess</a><br></br>
+              <a href="#" className="topics">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Clue</a><br></br>
+              <a href="#" className="topics">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Exploding Kittens</a><br></br>
+              <a href="#" className="topics">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Monopoly</a><br></br>
+              <a href="#" className="topics">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Pandemic</a><br></br>
+              <a href="#" className="topics">&nbsp;&nbsp;<img src={placeImg}></img>&nbsp;&nbsp;Scrabble</a><br></br>
             </div>
           </div>
           
@@ -35,7 +42,7 @@ export default function Blog() {
           </div>
 
           
-          <div className="discPost1">
+          <div className="discPost1" onClick={openMenu}>
             <div className = "discPostText">
               <h2 id = "sample1">Tips on improving the chess rules</h2>
               <p>Let's say I wanted to talk about a chess play I made <a href = "#">linked here</a>. 
@@ -62,6 +69,7 @@ export default function Blog() {
                   <a href="#" className="tags">Old release</a>, &nbsp;
               </p>
             </div>
+            {post1State&&<div>You're looking healthy!?</div>}
           </div>
 
           <div className="discPost1">
