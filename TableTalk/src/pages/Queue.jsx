c39388f529dc75a95a94f8d5658d9187e7d5b1cf
gabooshify <146407320+gabooshify@users.noreply.github.com>
2025-03-12 19:59:37 -0600

    Colored some cards, fixed 'ignore' button

--- TableTalk/src/pages/Queue.jsx
+++ TableTalk/src/pages/Queue.jsx
@@ -9,10 +9,14 @@ export default function Queue() {
     setRangeValue(e.target.value);
   };
 
+  const removeDiv = (btn) => {
+    ((btn.parentNode).parentNode).removeChild(btn.parentNode);
+  };
+
   return (
     <div className="container">
       <div className="options-container">
-        <div className="tall-card">
+        <div className="tall-card-blue">
           <h1>Options</h1>
           <form>
             <button className="queue-button">Edit Preferences</button>
@@ -27,10 +31,10 @@ export default function Queue() {
           <h1>Find people to play with!</h1>
           <form>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="fname" class="form-control" /><br />
+            <input type="text" id="name" name="fname" className="form-control" /><br />
 
             <label htmlFor="gametypes">Game Type</label>
-            <select name="Game Types" id="gametypes" class="form-control"><br />
+            <select name="Game Types" id="gametypes" className="form-control"><br />
               <option value="competitive">Competitive</option>
               <option value="competitive">Casual</option>
             </select><br />
@@ -50,33 +54,31 @@ export default function Queue() {
       </div>
 
       <div className="matches-container">
-        <div className="tall-card">
+        <div className="tall-card-blue">
           <h1>Matches For You</h1>
-            <form>
+          <form>
             <div className="match-card">
               <h1>Joe Shmoe</h1>
               <r1>You both like Competitive Games</r1><br></br>
               <r2>You both own Pandemic</r2><br></br><br></br>
               <button className="match-button">Message</button>
-              <button className="match-button">Ignore</button>
-              </div>
+              <button className="match-button" onClick={(e) => removeDiv(e.target)}>Ignore</button>
+            </div>
             <div className="match-card">
               <h1>James Games</h1>
               <r1>You're only 20 km away</r1><br></br>
               <r2>You both like Strategy Games</r2><br></br>
               <button className="match-button">Message</button>
-              <button className="match-button">Ignore</button>
-              </div>
+              <button className="match-button" onClick={(e) => removeDiv(e.target)}>Ignore</button>
+            </div>
             <div className="match-card">
               <h1>Jess Chess</h1>
               <r1>You're both friends with Hannah Montana</r1><br></br>
               <r2>You both like Strategy Games</r2><br></br>
               <button className="match-button">Message</button>
-              <button className="match-button">Ignore</button>
-              </div>
-            
-
-            </form>
+              <button className="match-button" onClick={(e) => removeDiv(e.target)}>Ignore</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
