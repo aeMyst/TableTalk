6d71de5d36aa6eafff22c7f459b85f8610752e1b
Peter <peter.tran2@ucalgary.ca>
2025-04-03 03:53:01 -0600

    fixed blog layout

--- TableTalk/src/pages/Blog.jsx
+++ TableTalk/src/pages/Blog.jsx
@@ -1,129 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../elements/card.css";
 import "./Blog.css";
-import placeImg from "../assets/svg/imagePlaceholder.svg";
-import dislike from "../assets/svg/dislike.svg";
-import like from "../assets/svg/like.svg";
-import viewImg from "../assets/svg/view.svg";
-import replyImg from "../assets/svg/reply.svg";
+import blogPosts from "../database/blogData.jsx";
+
+const Post = ({ title, excerpt, author, date }) => (
+  <article className="blog-post-card">
+    <div className="blog-post-content">
+      <h2 className="blog-post-title">{title}</h2>
+      <p className="blog-post-excerpt">{excerpt}</p>
+      <div className="blog-post-meta">
+        <span className="blog-author">By {author}</span>
+        <span className="blog-date">{date}</span>
+      </div>
+    </div>
+    <div className="blog-post-footer"></div>
+  </article>
+);
 
 export default function Blog() {
-  
+  const [sortOrder, setSortOrder] = useState("newest");
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 3;
+
+  const sortedPosts = [...blogPosts].sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return sortOrder === "newest" ? dateB - dateA : dateA - dateB;
+  });
+
+  const totalPages = Math.ceil(sortedPosts.length / postsPerPage);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = sortedPosts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="blog-container">
-      <div className="card">
-
-          <div className="sidebar">
-            <div className ="sidebarTitle">
-              <h1 className ="sidebarTitleText">Topics</h1>
-            </div>
-            <div className="sidebarContent">
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Casting Shadows</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Catan</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Chess</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Clue</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Exploding Kittens</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Monopoly</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Pandemic</a><br></br>
-              <a href="#" className="topics"><img src={placeImg}></img>&nbsp;&nbsp;Scrabble</a><br></br>
-            </div>
-          </div>
-          
-          <div className="mainTitle">
-            <h1>Blog Posts</h1>
+      <div className="blog-content-wrapper">
+        {/* Controls Section */}
+        <div className="blog-controls">
+          <div className="blog-search-box">
+            <h2 className="blog-search-title">Search</h2>
+            <input
+              type="text"
+              placeholder="Search blog posts..."
+              className="blog-search-input"
+            />
           </div>
 
-          
-          <div className="discPost1">
-            <div className = "discPostText">
-              <h2 id = "sample1">Tips on improving the chess rules</h2>
-              <p>Let's say I wanted to talk about a chess play I made <a href = "#">linked here</a>. 
-              I was wondering what happens if I change the rules to say 
-              all pieces move in a straight line.</p>
-            </div>
-            <div className = "discPostBar">
-              <div className = "discPostButtons">
-                <div className = "discPostButtonsLeft">
-                  <button className="buttons"><img src={viewImg} id="likeButton"></img></button>
-                  <button className="buttons"><img src={replyImg} id="likeButton"></img></button>
-                </div>
-                <div className = "discPostButtonsRight">
-                  <button className="buttons"><img src={like} id="likeButton"></img></button>
-                  <button className="buttons"><img src={dislike} id="likeButton"></img></button>
-                  </div>
-              </div>
-              <br></br>
-              <p>Tags: &nbsp;
-                  <a href="#" className="tags">Chess</a>, &nbsp;
-                  <a href="#" className="tags">New release</a>, &nbsp;
-                  <a href="#" className="tags">Cat</a>, &nbsp; 
-                  <a href="#" className="tags">Rules</a>, &nbsp;
-                  <a href="#" className="tags">Old release</a>, &nbsp;
-              </p>
-            </div>
+          <div className="blog-sort-box">
+            <h2 className="blog-sort-title">Sort By</h2>
+            <select
+              className="blog-sort-dropdown"
+              value={sortOrder}
+              onChange={(e) => setSortOrder(e.target.value)}
+            >
+              <option value="newest">Newest First</option>
+              <option value="oldest">Oldest First</option>
+            </select>
           </div>
+        </div>
 
-          <div className="discPost1">
-            <div className = "discPostText">
-              <h2 id = "sample1">Critique of the chess rules</h2>
-              <p>Let's say I wanted to talk about a chess play I made <a href = "#">linked here</a>. 
-              I was wondering what happens if I change the rules to say 
-              all pieces move in a straight line.</p>
+        {/* Blog Content Section */}
+        <div className="blog-layout">
+          <main className="blog-main">
+            <div className="blog-heading-row">
+              <h1 className="blog-heading">Blog Posts</h1>
+              <button className="blog-add-button">+ Add New Blog Post</button>
             </div>
-            <div className = "discPostBar">
-              <div className = "discPostButtons">
-                <div className = "discPostButtonsLeft">
-                  <button className="buttons"><img src={viewImg} id="likeButton"></img></button>
-                  <button className="buttons"><img src={replyImg} id="likeButton"></img></button>
-                </div>
-                <div className = "discPostButtonsRight">
-                  <button className="buttons"><img src={like} id="likeButton"></img></button>
-                  <button className="buttons"><img src={dislike} id="likeButton"></img></button>
-                  </div>
-              </div>
-              <br></br>
-              <p>Tags: &nbsp;
-                  <a href="#" className="tags">Chess</a>, &nbsp;
-                  <a href="#" className="tags">New release</a>, &nbsp;
-                  <a href="#" className="tags">Cat</a>, &nbsp; 
-                  <a href="#" className="tags">Rules</a>, &nbsp;
-                  <a href="#" className="tags">Old release</a>, &nbsp;
-              </p>
+            <div className="blog-post-list">
+              {currentPosts.map((post) => (
+                <Post
+                  key={post.id}
+                  title={post.title}
+                  excerpt={post.excerpt}
+                  author={post.author}
+                  date={post.date}
+                />
+              ))}
             </div>
-          </div>
-          
-          <div className="discPost1">
-            <div className = "discPostText">
-              <h2 id = "sample1">I am unsure on the rules of chess</h2>
-              <p>Let's say I wanted to talk about a chess play I made <a href = "#">linked here</a>. 
-              I was wondering what happens if I change the rules to say 
-              all pieces move in a straight line.</p>
-            </div>
-            <div className = "discPostBar">
-              <div className = "discPostButtons">
-                <div className = "discPostButtonsLeft">
-                  <button className="buttons"><img src={viewImg} id="likeButton"></img></button>
-                  <button className="buttons"><img src={replyImg} id="likeButton"></img></button>
-                </div>
-                <div className = "discPostButtonsRight">
-                  <button className="buttons"><img src={like} id="likeButton"></img></button>
-                  <button className="buttons"><img src={dislike} id="likeButton"></img></button>
-                  </div>
-              </div>
-              <br></br>
-              <p>Tags: &nbsp;
-                  <a href="#" className="tags">Chess</a>, &nbsp;
-                  <a href="#" className="tags">New release</a>, &nbsp;
-                  <a href="#" className="tags">Cat</a>, &nbsp; 
-                  <a href="#" className="tags">Rules</a>, &nbsp;
-                  <a href="#" className="tags">Old release</a>, &nbsp;
-              </p>
-            </div>
-          </div>
-          
 
+            {/* Pagination Section */}
+            <div className="pagination">
+              {Array.from({ length: totalPages }, (_, i) => (
+                <button
+                  key={i + 1}
+                  className={`page-button ${currentPage === i + 1 ? "active" : ""}`}
+                  onClick={() => handlePageChange(i + 1)}
+                >
+                  {i + 1}
+                </button>
+              ))}
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
-} 
-
+}
